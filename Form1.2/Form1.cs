d6926382e3d5ee2546f527ff6063c74df1039e6c
user <user@WIN-2FA9I1VU4LK>
2023-02-13 13:47:01 +0400

    Add project files.

---
+++ Form1.2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            Random rnd = new Random();
+            int a = rnd.Next(0, 720);
+            int b = rnd.Next(0, 256);
+            button1.Location = new Point(a,b);
+        }
+    }
+}
